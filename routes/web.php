86b6cdd42217a48c2782e5244f367a4ec8a47904
Alvaro <aproposito@gmail.com>
2026-05-12 20:43:50 +0200

    feat: add resource routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ChampionPredictionController;
+use App\Http\Controllers\MatchGameController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\MatchPredictionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('teams', TeamController::class);
+Route::resource('matches', MatchGameController::class);
+Route::resource('match_predictions', MatchPredictionController::class);
+Route::resource('users', UserController::class);
+Route::resource('champion_predictions', ChampionPredictionController::class);
